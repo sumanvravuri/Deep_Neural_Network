fixed dumb for loop problem in gradient calculation that made cpu and gpu versions run 5x slower than they should

src/deep_neural_net_cuda.py
@@ -572,7 +572,7 @@ class Neural_Network(object, Vector_Math):
             sys.exit()
     def read_label_file(self): #completed
         try:
-            return sp.loadmat(self.label_file_name)['labels'] #in MATLAB format
+            return sp.loadmat(self.label_file_name)['labels'].astype(int) #in MATLAB format
         except IOError:
             print "Unable to open ", self.label_file_name, "... Exiting now"
             sys.exit()
@@ -767,7 +767,7 @@ class NN_Tester(Neural_Network): #completed
     def write_posterior_prob_file(self): #completed
         try:
             print "Writing to", self.output_name
-            sp.savemat(self.output_name,{'targets' : self.posterior_probs}, oned_as='column') #output name should have .mat extension
+            sp.savemat(self.output_name,{'targets' : self.posterior_probs.as_numpy_array()}, oned_as='column') #output name should have .mat extension
         except IOError:
             print "Unable to write to ", self.output_name, "... Exiting now"
             sys.exit()
@@ -1037,7 +1037,7 @@ class NN_Trainer(Neural_Network):
         if self.l2_regularization_const > 0.0:
             print "regularized loss is", loss
         print "number correctly classified is", num_correct, "of", num_examples
-
+        fat_labels = np.zeros((self.backprop_batch_size, np.max(self.labels) + 1))
         for epoch_num in range(len(self.steepest_learning_rate)):
             print "At epoch", epoch_num+1, "of", len(self.steepest_learning_rate), "with learning rate", self.steepest_learning_rate[epoch_num]
             batch_index = 0
@@ -1050,9 +1050,13 @@ class NN_Trainer(Neural_Network):
                 hiddens = self.forward_first_order_methods(self.features[batch_index:end_index], self.model)
                 #calculating negative gradient of log softmax
                 weight_vec = -hiddens[self.model.num_layers] #batchsize x n_outputs
-                for label_index in range(batch_index,end_index):
-                    data_index = label_index - batch_index
-                    weight_vec[data_index, int(self.labels[label_index])] += 1 #the int is to enforce proper indexing
+                batch_labels = self.labels[batch_index:end_index]
+                fat_labels.flat[np.arange(0,weight_vec.shape[1] * batch_labels.size, weight_vec.shape[1]) + batch_labels.ravel()] = 1.
+                weight_vec -= fat_labels[:batch_size,:]
+                fat_labels.flat[np.arange(0,weight_vec.shape[1] * batch_labels.size, weight_vec.shape[1]) + batch_labels.ravel()] = 0.
+#                for label_index in range(batch_index,end_index):
+#                    data_index = label_index - batch_index
+#                    weight_vec[data_index, int(self.labels[label_index])] += 1 #the int is to enforce proper indexing
                 #averaging batches
                 bias_update = gnp.sum(weight_vec, axis=0)
                 weight_update = gnp.dot(hiddens[self.model.num_layers-1].T, weight_vec)
@@ -1065,7 +1069,7 @@ class NN_Trainer(Neural_Network):
                     bias_cur_layer = str(layer_num)
                     bias_next_layer = str(layer_num+1)
                     weight_vec = gnp.dot(weight_vec, self.model.weights[weight_next_layer].T) * hiddens[layer_num] * (1-hiddens[layer_num]) #n_hid x n_out * (batchsize x n_out), do the biases get involved in this calculation???
-                    gnp.free_reuse_cache(False)
+#                    gnp.free_reuse_cache(False)
                     self.model.weights[weight_next_layer] += self.steepest_learning_rate[epoch_num] / batch_size * (weight_update - self.l2_regularization_const * self.model.weights[weight_next_layer])
                     self.model.bias[bias_next_layer][0] += self.steepest_learning_rate[epoch_num] / batch_size * (bias_update - self.l2_regularization_const * self.model.bias[bias_next_layer][0])
                     del bias_update
@@ -1463,9 +1467,9 @@ class NN_Trainer(Neural_Network):
 #        print "Amount of memory after first order methods in use is", gnp.memory_in_use(True), "MB"
         #derivative of log(cross-entropy softmax)
         weight_vec = hiddens[model.num_layers] #batchsize x n_outputs
-
-        for index in range(batch_size):
-            weight_vec[index, int(batch_labels[index])] -= 1
+        fat_labels = np.zeros(weight_vec.shape)
+        fat_labels.flat[np.arange(0,weight_vec.shape[1] * batch_labels.size, weight_vec.shape[1]) + batch_labels.ravel()] = 1.
+        weight_vec -= fat_labels
 #        print "Amount of memory before backward pass in use is", gnp.memory_in_use(True), "MB"
         gradient_weights = self.backward_pass(weight_vec, hiddens, model)
         if clean_hiddens_flag:
@@ -2062,11 +2066,10 @@ class NN_Trainer(Neural_Network):
             model = self.model
         
         excluded_keys = {'bias': ['0'], 'weights': []}
-        
         if self.do_backprop == False:
-            classification_batch_size = 4096
+            classification_batch_size = 8192
         else:
-            classification_batch_size = max(self.backprop_batch_size, 4096)
+            classification_batch_size = max(self.backprop_batch_size, 8192)
         
         batch_index = 0
         end_index = 0

src/deep_neural_network.py
@@ -17,6 +17,7 @@ import sys
 import numpy as np
 import scipy.io as sp
 import scipy.linalg as sl
+from scipy.special import expit
 import scipy.optimize as sopt
 import math
 import copy
@@ -25,7 +26,8 @@ import argparse
 class Vector_Math:
     #math functions
     def sigmoid(self,inputs): #completed, expensive, should be compiled
-        return 1./(1+np.exp(-inputs)) #1/(1+e^-X)
+        return expit(inputs)
+        #return 1./(1.+np.exp(-inputs)) #1/(1+e^-X)
     def softmax(self, inputs): #completed, expensive, should be compiled
         #subtracting max value of each data point below for numerical stability
         #exp_inputs = np.exp(inputs - np.transpose(np.tile(np.max(inputs,axis=1), (inputs.shape[1],1))))
@@ -36,7 +38,11 @@ class Vector_Math:
         #print "weight dims are ", weights.shape
         #print "bias dims are ", biases.shape
         #return np.dot(inputs,weights)+np.tile(biases, (inputs.shape[0],1))
-        return np.dot(inputs,weights) + biases#[np.newaxis, :]
+#        return slb.cblas.dgemm(alpha=1.0, a=inputs, b=weights) + biases
+        out = np.dot(inputs,weights)
+        out += biases
+        return out
+#        return np.dot(inputs,weights) + biases#[np.newaxis, :]
 class Neural_Network_Weight(object):
     def __init__(self, num_layers=0, weights=None, bias=None, weight_type=None):
         #num_layers
@@ -184,15 +190,15 @@ class Neural_Network_Weight(object):
                 print "no layers found. Need at least one layer... Exiting now"
                 sys.exit()
         try:
-            self.bias['0'] = weight_dict['bias0']
+            self.bias['0'] = np.array(weight_dict['bias0'], order='C')
         except KeyError:
             print "bias0 not found. bias0 must exist for", weight_matrix_name, "to be a valid weight file... Exiting now"
             sys.exit()
         for layer_num in range(1,self.num_layers+1): #changes weight layer type to ascii string, which is what we'll need for later functions
             weight_cur_layer = ''.join([str(layer_num-1),str(layer_num)])
             bias_cur_layer = str(layer_num)
-            self.weights[weight_cur_layer] = weight_dict[''.join(['weights', weight_cur_layer])]
-            self.bias[bias_cur_layer] = weight_dict[''.join(['bias', bias_cur_layer])]  
+            self.weights[weight_cur_layer] = np.array(weight_dict[''.join(['weights', weight_cur_layer])], order='C')
+            self.bias[bias_cur_layer] = np.array(weight_dict[''.join(['bias', bias_cur_layer])], order='C')
             self.weight_type[weight_cur_layer] = weight_dict[''.join(['weights',weight_cur_layer,'_type'])][0].encode('ascii', 'ignore')
         del weight_dict
         self.check_weights()
@@ -565,13 +571,13 @@ class Neural_Network(object, Vector_Math):
                 return default_value
     def read_feature_file(self): #completed
         try:
-            return sp.loadmat(self.feature_file_name)['features'] #in MATLAB format
+            return np.array(sp.loadmat(self.feature_file_name)['features'], order='C') #in MATLAB format
         except IOError:
             print "Unable to open ", self.feature_file_name, "... Exiting now"
             sys.exit()
     def read_label_file(self): #completed
         try:
-            return sp.loadmat(self.label_file_name)['labels'] #in MATLAB format
+            return np.array(sp.loadmat(self.label_file_name)['labels'], order='C') #in MATLAB format
         except IOError:
             print "Unable to open ", self.label_file_name, "... Exiting now"
             sys.exit()
@@ -1889,9 +1895,9 @@ class NN_Trainer(Neural_Network):
         excluded_keys = {'bias': ['0'], 'weights': []}
         
         if self.do_backprop == False:
-            classification_batch_size = 4096
+            classification_batch_size = 32768
         else:
-            classification_batch_size = max(self.backprop_batch_size, 4096)
+            classification_batch_size = max(self.backprop_batch_size, 32768)
         
         batch_index = 0
         end_index = 0